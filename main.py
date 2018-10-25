885950ef4603c8620d64eca138b3400fb1ad0185
Geoff Wakefield <g.wakefield@hotmail.com>
2018-10-25 11:35:06 -0500

    added random spider response

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import discord
 import logging
+import random
 import sys
 
 from discord.ext import commands
@@ -18,6 +19,9 @@ TOKEN = os.environ.get('TOKEN')
 
 bot = commands.Bot(command_prefix='!', description='A Gravy bot for the people')
 
+def hasAttentionSpan():
+    return random.random() > 0.05
+
 @bot.event
 async def on_ready():
     print('Logged in as')
@@ -29,63 +33,105 @@ async def on_ready():
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send(f'Yo {ctx.message.author.name}, keep it gangsta')
+    if hasAttentionSpan():
+        await ctx.send(f'Yo {ctx.message.author.name}, keep it gangsta')
+    else:
+        await ctx.send('Hel-.....spiders!!')
 
 @bot.command()
 async def gravy(ctx):
-    await ctx.send('Gravy for President!')
+    if hasAttentionSpan():
+        await ctx.send('Gravy for President!')
+    else:
+        await ctx.send('Spiders!!')
 
 @bot.command()
 async def gravylife(ctx):
-    await ctx.send('My friends call me \'G\' because I was a dark gangster from Kimberly Drive. I had a Raiders trench coat that put Juice\'s trench coat to shame. I was OG, ya know.')
+    if hasAttentionSpan():
+        await ctx.send('My friends call me \'G\' because I was a dark gangster from Kimberly Drive. I had a Raiders trench coat that put Juice\'s trench coat to shame. I was OG, ya know.')
+    else:
+        await ctx.send('My friends call me...wait....spiders!!')
 
 @bot.command()
 async def playgame(ctx):
-    await ctx.send('INDIFFERENCE TO GAME!')
+    if hasAttentionSpan():
+        await ctx.send('INDIFFERENCE TO GAME!')
+    else:
+        await ctx.send('INDIFFERENCE T-....SPIDERS!')
 
 @bot.command()
 async def favoritemovie(ctx):
-    await ctx.send('Rush (with totally naked butts)')
+    if hasAttentionSpan():
+        await ctx.send('Rush (with totally naked butts)')
+    else:
+        await ctx.send('Rush, with totally...hey, spiders!')
 
 @bot.command()
 async def trenchaf(ctx):
-    await ctx.send('https://media.giphy.com/media/cA7kvk12D4rvBeb4rR/giphy.gif')
+    if hasAttentionSpan():
+        await ctx.send('https://media.giphy.com/media/cA7kvk12D4rvBeb4rR/giphy.gif')
+    else:
+        await ctx.send('Hel-.....spiders!!')
 
 @bot.command()
 async def gravybeerface(ctx):
-    await ctx.send('https://media.giphy.com/media/PNwk9K1FVvWyw1QDcz/giphy.gif')
+    if hasAttentionSpan():
+        await ctx.send('https://media.giphy.com/media/PNwk9K1FVvWyw1QDcz/giphy.gif')
+    else:
+        await ctx.send('https://media.giphy.com/media/26gsr8eaTjRiIxKgM/giphy.gif')
 
 @bot.command()
 async def info(ctx):
-    embed = discord.Embed(title="Gravy Bot", description="To get your daily gravy fix", color=0xeee657)
+    if hasAttentionSpan():
+        embed = discord.Embed(title="Gravy Bot", description="To get your daily gravy fix", color=0xeee657)
 
-    # give info about you here
-    embed.add_field(name="Author", value="fingedevil")
+        # give info about you here
+        embed.add_field(name="Author", value="fingedevil")
 
-    # Shows the number of servers the bot is member of.
-    embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
+        # Shows the number of servers the bot is member of.
+        embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
 
-    # Shows link to GitHub code
-    embed.add_field(name="GitHub", value="https://github.com/fringedevil/gravy-bot")
+        # Shows link to GitHub code
+        embed.add_field(name="GitHub", value="https://github.com/fringedevil/gravy-bot")
 
-    await ctx.send(embed=embed)
+        await ctx.send(embed=embed)
+    else:
+        embed = discord.Embed(title="Gravy Spider", description="To get your daily spider fix", color=0xeee657)
+        embed.add_field(name="Author", value="fingespider")
+        embed.add_field(name="Spider count", value=f"{len(bot.guilds)}")
+        embed.add_field(name="SpideyHub", value="https://github.com/fringedevil/gravy-bot")
+        await ctx.send(embed=embed)
 
 bot.remove_command('help')
 
 @bot.command()
 async def help(ctx):
-    embed = discord.Embed(title="Gravy Bot", description="MOAR Gravy!! List of commands are:", color=0xeee657)
+    if hasAttentionSpan():
+        embed = discord.Embed(title="Gravy Bot", description="MOAR Gravy!! List of commands are:", color=0xeee657)
 
-    embed.add_field(name="!hello", value="Say hi to gravy-bot", inline=False)
-    embed.add_field(name="!gravy", value="A vote of confidence for Gravy", inline=False)
-    embed.add_field(name="!gravylife", value="The long and winding road to becoming Gravy", inline=False)
-    embed.add_field(name="!playgame", value="Ask Gravy bot to play a game", inline=False)
-    embed.add_field(name="!favoritemovie", value="Ask Gravy bot what his favorite movie is", inline=False)
-    embed.add_field(name="!trenchaf", value="Gravy at peak performance", inline=False)
-    embed.add_field(name="!gravybeerface", value="Vintage Gravy", inline=False)
-    embed.add_field(name="!info", value="Details on the bot that is Gravy", inline=False)
+        embed.add_field(name="!hello", value="Say hi to gravy-bot", inline=False)
+        embed.add_field(name="!gravy", value="A vote of confidence for Gravy", inline=False)
+        embed.add_field(name="!gravylife", value="The long and winding road to becoming Gravy", inline=False)
+        embed.add_field(name="!playgame", value="Ask Gravy bot to play a game", inline=False)
+        embed.add_field(name="!favoritemovie", value="Ask Gravy bot what his favorite movie is", inline=False)
+        embed.add_field(name="!trenchaf", value="Gravy at peak performance", inline=False)
+        embed.add_field(name="!gravybeerface", value="Vintage Gravy", inline=False)
+        embed.add_field(name="!info", value="Details on the bot that is Gravy", inline=False)
 
-    await ctx.send(embed=embed)
+        await ctx.send(embed=embed)
+    else:
+        embed = discord.Embed(title="Spider Bot", description="MOAR Spiders!! List of spiders are:", color=0xeee657)
+
+        embed.add_field(name="!hello", value="Say hi to spidey-bot", inline=False)
+        embed.add_field(name="!gravy", value="A vote of spiders for Gravy", inline=False)
+        embed.add_field(name="!gravylife", value="The long and winding road to becoming a spider", inline=False)
+        embed.add_field(name="!playgame", value="Ask spidey bot to play a game", inline=False)
+        embed.add_field(name="!favoritemovie", value="Ask Gravy bot what his favorite spider is", inline=False)
+        embed.add_field(name="!trenchaf", value="Gravy at spiderman performance", inline=False)
+        embed.add_field(name="!gravybeerface", value="Spider Gravy", inline=False)
+        embed.add_field(name="!info", value="Details on the spider that is Gravy", inline=False)
+
+        await ctx.send(embed=embed)
 
 # Run bot
 bot.run(TOKEN)
